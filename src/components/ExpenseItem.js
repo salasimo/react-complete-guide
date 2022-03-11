794e9957540732be7be18f00a70dce77e22c8015
Simone <simonesala@me.com>
2022-03-11 13:38:52 +0100

    css class fixed

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,10 +3,12 @@ import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
     return (
-        <div className="expense-item__description">
-            <ExpenseDate date={props.date} />
-            <h2>{props.title}</h2>
-            <div className="expense-item__price ">${props.amount}</div>
+        <div className="expense-item">
+            <div className="expense-item__description">
+                <ExpenseDate date={props.date} />
+                <h2>{props.title}</h2>
+                <div className="expense-item__price ">${props.amount}</div>
+            </div>
         </div>
     );
 }
