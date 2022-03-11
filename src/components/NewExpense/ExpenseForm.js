c6c94c69da1029a02fb14b51dbadf9a9344c1472
Simone <simonesala@me.com>
2022-03-11 17:42:45 +0100

    multiple states

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = (props) => {
+    const [enteredTitle, setEnteredTitle] = useState("");
+    const titleChangeHandler = (e) => {
+        setEnteredTitle(e.target.value);
+    };
+    const [enteredAmount, setEnteredAmount] = useState("");
+    const amountChangeHandler = (e) => {
+        setEnteredAmount(e.target.value);
+    };
+    const [enteredDate, setEnteredDate] = useState("");
+    const dateChangeHandler = (e) => {
+        setEnteredDate(e.target.value);
+    };
+
+    return (
+        <form>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>Title</label>
+                    <input type="text" onChange={titleChangeHandler}></input>
+                </div>
+                <div className="new-expense__control">
+                    <label>Amount</label>
+                    <input
+                        onChange={amountChangeHandler}
+                        type="number"
+                        min="0.01"
+                        step="0.01"
+                    ></input>
+                </div>
+                <div className="new-expense__control">
+                    <label>Date</label>
+                    <input
+                        onChange={dateChangeHandler}
+                        type="date"
+                        min="2019-01-01"
+                        max="2022-12-31"
+                    ></input>
+                </div>
+            </div>
+            <div className="new-expense__actions">
+                <button type="submit">Add Expense</button>
+            </div>
+        </form>
+    );
+};
+export default ExpenseForm;
