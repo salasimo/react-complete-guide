1dc3e1877a38967e66d39ea578f74b2cc0ef2cf0
Simone <simonesala@me.com>
2022-03-11 17:58:33 +0100

    submit form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -37,8 +37,18 @@ const ExpenseForm = (props) => {
         enteredDate: "",
     });
 
+    const submitHandler = (e) => {
+        e.preventDefault();
+        const expenseData = {
+            title: userInput.enteredTitle,
+            amount: userInput.enteredAmount,
+            date: new Date(userInput.enteredDate),
+        };
+        console.log(expenseData);
+    };
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
