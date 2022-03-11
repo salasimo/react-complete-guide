41b4d885d3193c4b5c5d0d00970c869791240c6f
Simone <simonesala@me.com>
2022-03-11 13:29:31 +0100

    ex fixed

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,7 +4,7 @@ import "./ExpenseItem.css";
 function ExpenseItem(props) {
     return (
         <div className="expense-item__description">
-            <ExpenseDate date={props.ExpenseDate} />
+            <ExpenseDate date={props.date} />
             <h2>{props.title}</h2>
             <div className="expense-item__price ">${props.amount}</div>
         </div>
