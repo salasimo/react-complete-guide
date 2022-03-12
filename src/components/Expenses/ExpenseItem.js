1be62b45d473091462b0fc74a4e8bdd467297170
Simone <simonesala@me.com>
2022-03-12 14:09:16 +0100

    removed btn test

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,22 +5,13 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-    const [title, setTitle] = useState(props.title);
-    // let title = props.title;
-
-    const clickHandler = () => {
-        // title = "New Title......";
-        setTitle("New Title......");
-    };
-
     return (
         <Card className="expense-item">
             <div className="expense-item__description">
                 <ExpenseDate date={props.date} />
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price ">${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
