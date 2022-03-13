c746e78edffc0f13c11c2dbdcc4dabaf7123ce62
Simone <simonesala@me.com>
2022-03-14 00:09:23 +0100

    style bar - finish app

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 
 function Expenses(props) {
@@ -21,6 +22,7 @@ function Expenses(props) {
                     selected={filteredYear}
                     onFilterYear={filterChangeHandler}
                 />
+                <ExpensesChart expenses={filteredExpenses} />
                 <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
