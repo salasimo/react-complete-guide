63c05dc4f34b18300bfac432c3df527211461e5a
Simone <simonesala@me.com>
2022-03-13 12:32:24 +0100

    statefull list and conditional content

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
+import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+const DUMMY_EXPENSES = [
+    {
+        id: "1",
+        title: "Car rent",
+        amount: 123.67,
+        date: new Date(2022, 2, 28),
+    },
+    {
+        id: "2",
+        title: "Insurance",
+        amount: 422.89,
+        date: new Date(2022, 1, 24),
+    },
+    {
+        id: "3",
+        title: "Food and drinks",
+        amount: 90.12,
+        date: new Date(2021, 2, 12),
+    },
+    {
+        id: "4",
+        title: "Doctor visit",
+        amount: 76.75,
+        date: new Date(2020, 4, 2),
+    },
+];
 const App = () => {
-    const expenses = [
-        {
-            id: "1",
-            title: "Car rent",
-            amount: 123.67,
-            date: new Date(2022, 2, 28),
-        },
-        {
-            id: "2",
-            title: "Insurance",
-            amount: 422.89,
-            date: new Date(2019, 1, 24),
-        },
-        {
-            id: "3",
-            title: "Food and drinks",
-            amount: 90.12,
-            date: new Date(2021, 2, 12),
-        },
-        {
-            id: "4",
-            title: "Doctor visit",
-            amount: 76.75,
-            date: new Date(2020, 4, 2),
-        },
-    ];
-
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
     const addExpenseHandler = (expense) => {
-        console.log(expense);
+        setExpenses((prevExpenses) => {
+            return [expense, ...prevExpenses];
+        });
     };
+
     return (
         <div>
             <NewExpense onAddExpense={addExpenseHandler} />
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,10 +5,14 @@ import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 function Expenses(props) {
-    const [filteredYear, setFilteredYear] = useState("0");
+    const [filteredYear, setFilteredYear] = useState("2022");
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     };
+
+    const filteredExpenses = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
     return (
         <div>
             <Card className="expenses">
@@ -16,26 +20,18 @@ function Expenses(props) {
                     selected={filteredYear}
                     onFilterYear={filterChangeHandler}
                 />
-                <ExpenseItem
-                    title={props.items[0].title}
-                    amount={props.items[0].amount}
-                    date={props.items[0].date}
-                />
-                <ExpenseItem
-                    title={props.items[1].title}
-                    amount={props.items[1].amount}
-                    date={props.items[1].date}
-                />
-                <ExpenseItem
-                    title={props.items[2].title}
-                    amount={props.items[2].amount}
-                    date={props.items[2].date}
-                />
-                <ExpenseItem
-                    title={props.items[3].title}
-                    amount={props.items[3].amount}
-                    date={props.items[3].date}
-                />
+                {filteredExpenses.length === 0 ? (
+                    <p>No expenses found</p>
+                ) : (
+                    filteredExpenses.map((expense) => (
+                        <ExpenseItem
+                            key={expense.id}
+                            title={expense.title}
+                            amount={expense.amount}
+                            date={expense.date}
+                        />
+                    ))
+                )}
             </Card>
         </div>
     );
