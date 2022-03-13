2beaf4b0959e9d2f0e562ae3ef8b360040c42d4f
Simone <simonesala@me.com>
2022-03-13 12:37:56 +0100

    refactoring

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,6 +13,18 @@ function Expenses(props) {
     const filteredExpenses = props.items.filter((expense) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
+
+    let expensesContent = <p>No expenses found</p>;
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+        ));
+    }
     return (
         <div>
             <Card className="expenses">
@@ -20,16 +32,7 @@ function Expenses(props) {
                     selected={filteredYear}
                     onFilterYear={filterChangeHandler}
                 />
-                {filteredExpenses.length === 0 && <p>No expenses found</p>}
-                {filteredExpenses.length > 0 &&
-                    filteredExpenses.map((expense) => (
-                        <ExpenseItem
-                            key={expense.id}
-                            title={expense.title}
-                            amount={expense.amount}
-                            date={expense.date}
-                        />
-                    ))}
+                {expensesContent}
             </Card>
         </div>
     );
